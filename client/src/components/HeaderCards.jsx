76f5e564793f869040f9b8d1dd2115187ec3782a
TharunRaj <66259876+TharunRaj06@users.noreply.github.com>
2023-07-23 14:41:09 +0530

    commits

---
+++ client/src/components/HeaderCards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const HeaderCards = () => {
+  return (
+    <div className="cards">
+      <div className="card">
+        <p>Card 1 content</p>
+      </div>
+      <div className="card">
+        <h3>Card 2</h3>
+        <p>Card 2 content</p>
+      </div>
+
+      <div className="card">
+        <h3>Card 3</h3>
+        <p>Card 3 content</p>
+      </div>
+
+      <div className="card">
+        <h3>Card 4</h3>
+        <p>Card 4 content</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCards;
