6c13451fe9da0e5b2b9019e6ad2b53c6f0c23cbf
TharunRaj <66259876+TharunRaj06@users.noreply.github.com>
2023-07-24 11:51:19 +0530

    server changes

--- Backend/server.js
+++ Backend/server.js
@@ -65,11 +65,18 @@ app.post("/verify-payment", async (req, res) => {
 
     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ message: "Payment successful" });
-  } catch (error) {
-    res.status(500).json({ error: "Payment verification failed" });
-  }
-});
+    // Send the purchase details to the admin
+    const mailOptionsAdmin = {
+        from: 'YOUR_EMAIL_ADDRESS', // Replace with your email address
+        to: 'ADMIN_EMAIL_ADDRESS', // Replace with the admin's email address
+        subject: 'New Purchase',
+        text: `A new purchase was made.\n\nName: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nCollege: ${college}\nCourse: ${selectedCourse}\nPrice: ${price}`,
+      };
+  
+      await transporter.sendMail(mailOptionsAdmin);
+  
+      // For demonstration purposes, we will just send a success response
+      res.status(200).json({ message: 'Payment successful' });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
