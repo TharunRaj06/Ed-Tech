3ff873e17ba8223edc4805d6cae72c0dec6d7409
TharunRaj <66259876+TharunRaj06@users.noreply.github.com>
2023-07-24 11:32:27 +0530

    server changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const Razorpay = require("razorpay");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = 3000; // Replace with your desired port
+const port = 3000;
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -14,9 +14,8 @@ const razorpay = new Razorpay({
   key_secret: "YOUR_RAZORPAY_KEY_SECRET", // Replace with your actual Key Secret
 });
 
-// Configure Nodemailer with your email service details
 const transporter = nodemailer.createTransport({
-  service: "Gmail", // Replace with your email service (e.g., Gmail, Outlook, etc.)
+  service: "Gmail",
   auth: {
     user: "YOUR_EMAIL_ADDRESS",
     pass: "YOUR_EMAIL_PASSWORD",
@@ -28,8 +27,6 @@ app.use(bodyParser.json());
 app.post("/create-payment", async (req, res) => {
   try {
     const { amount, currency, receipt, notes } = req.body;
-
-    // Create a Razorpay order
     const order = await razorpay.orders.create({
       amount: amount,
       currency: currency || "INR",
@@ -47,7 +44,6 @@ app.post("/verify-payment", async (req, res) => {
   try {
     const { orderId, paymentId, signature, email } = req.body;
 
-    // Verify the payment signature
     const attributes = `${orderId}|${paymentId}`;
     const isSignatureValid = razorpay.validateWebhookSignature(
       attributes,
@@ -58,20 +54,17 @@ app.post("/verify-payment", async (req, res) => {
       return res.status(403).json({ error: "Invalid signature" });
     }
 
-    // Fetch the payment details
     const payment = await razorpay.payments.fetch(paymentId);
 
-    // Process the payment and send confirmation email to the user
     const mailOptions = {
-      from: "YOUR_EMAIL_ADDRESS", // Replace with your email address
-      to: email, // Use the user's email address
+      from: "YOUR_EMAIL_ADDRESS",
+      to: email,
       subject: "Payment Confirmation",
       text: "Thank you for your purchase. Your payment was successful!",
     };
 
     await transporter.sendMail(mailOptions);
 
-    // For demonstration purposes, we will just send a success response
     res.status(200).json({ message: "Payment successful" });
   } catch (error) {
     res.status(500).json({ error: "Payment verification failed" });
